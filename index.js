b671f2be14443c5a3a52a3c11a4829ea2bee2fb5
Massimo <mprencipe@gmail.com>
2025-04-01 10:30:03 +0300

    this

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const { exec } = require("child_process");
-const express = require("express");
-const app = express();
-
-app.get("/run", (req, res) => {
-  const name = req.query.name;
-
-  exec(`echo Hello ${name}`, (err, stdout, stderr) => {
-    if (err) {
-      res.status(500).send("Error occurred");
-      return;
-    }
-    res.send(`Result: ${stdout}`);
-  });
-});
-
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
-});
+const crypto = require("crypto");
+function hashPassword(password) {
+    var hasher = crypto.createHash('md5');
+    var hashed = hasher.update(password).digest("hex"); // BAD
+    return hashed;
+}
