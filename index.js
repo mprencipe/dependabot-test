32ba62378a4e354bf67c6d09578b6d29b52948d5
Massimo <mprencipe@gmail.com>
2025-04-01 10:22:33 +0300

    this

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-console.log('test');
+const http = require('http');
+const url = require('url');
+const { exec } = require('child_process');
 
-function insecurePassword() {
-    // BAD: the random suffix is not cryptographically secure
-    var suffix = Math.random();
-    var password = "thePassword" + suffix;
-    return password;
-}
+const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  const userInput = queryObject.cmd;
+
+  // VULNERABLE: Command injection
+  exec('ls ' + userInput, (error, stdout, stderr) => {
+    if (error) {
+      res.writeHead(500);
+      res.end(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      res.writeHead(500);
+      res.end(`Stderr: ${stderr}`);
+      return;
+    }
+    res.writeHead(200);
+    res.end(`Output: ${stdout}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
